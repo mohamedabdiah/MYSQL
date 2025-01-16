8658a9f46927c837a24173ce35668546f473afe0
mohamedabdiah <mohamedabdikaniahmed@gmail.com>
2025-01-16 15:17:50 +0300

    Day three

---
+++ creating database.sql
@@ -0,0 +1,1 @@
+create database classka_Duhurkii_January
---
+++ creating tables.sql
@@ -0,0 +1,30 @@
+create table emplooyees(
+employee_id Int,
+firstname varchar(255),
+lastname varchar(255),
+age int,
+department varchar(255),
+hire_date Date
+);
+
+drop table emplooyees
+
+create table users(
+      userid int,
+      username varchar(50),
+      email varchar(50),
+      password varchar(50)
+);
+
+create table students (
+      student_id int,
+      fullname varchar(50),
+      mothername varchar(50),
+      classname varchar(50),
+      teacher varchar(50),
+      enrollment_date Date
+);
+
+drop table emplooyees;
+drop table users;
+drop table students;

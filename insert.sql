88f142f82fbd1aebe463cdded796c3782ae37745
mohamedabdiah <mohamedabdikaniahmed@gmail.com>
2025-01-20 16:09:52 +0300

    Done

---
+++ insert.sql
@@ -0,0 +1,37 @@
+create table students(
+      id int primary key,
+      name text,
+      age int,
+      grade char(1)
+);
+
+select *from students
+-- Single rows
+insert into students(id,name,age,grade)
+values(1,'mowliid',30,'A')
+
+-- Multiple rows
+insert into students(id,name,age,grade)
+VALUES
+(2,'Amiin Cabdi Xasan',24,'B'),
+(3,'Xirsi Maxamuud Ciise',20,'A'),
+(4,'Sidiiq Ibraahim',35,'C')
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into students values(5,'Huda aadan Cali',18,'D')
+select *from students
+
+
+insert into students(id,name,age,grade)
+VALUES(a,)
